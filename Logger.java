f90209d9385972972f5fe2bd8f6805306063dd15
Aadhi2124 <145534025+Aadhi2124@users.noreply.github.com>
2025-06-18 19:31:06 +0530

    Add files via upload

---
+++ Logger.java
@@ -0,0 +1,20 @@
+package design_principles;
+
+public class Logger {
+	private static Logger logger;
+	
+	private Logger() {
+		
+	}
+
+	public static Logger getLogger() {
+		if(logger == null) {
+			logger = new Logger();
+		}
+		return logger;
+	}
+	
+	public void log(String username) {
+		System.out.println("[Logged] "+username);
+	}
+}
---
+++ Test.java
@@ -0,0 +1,20 @@
+package design_principles;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+		Logger l1 = Logger.getLogger();
+		Logger l2 = Logger.getLogger();
+		
+		l1.log("Aadhi");
+		
+		if(l1 == l2) {
+			System.out.println("Both Objects having same instance");
+		}
+		else {
+			System.out.println("Both are Different");
+		}
+	}
+
+}
